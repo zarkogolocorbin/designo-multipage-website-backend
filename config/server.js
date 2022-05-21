48c69487789c12451ba5e1f8b874fda754618df7
zarko <zarkogolocorbin@gmail.com>
2022-05-21 11:55:50 +0200

    add backand

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '02dd7f31158bbaf4a081b8edbbe41f4a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1473e94-fd18-4bc9-855b-c411b006ab3f'
+};
